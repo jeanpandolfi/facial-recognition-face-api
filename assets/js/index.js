fb46b99fb7c89abee4aa2c047f382ba6171de4f4
dBraga <douglasabraga@outlook.com>
2021-10-03 15:42:18 -0300

    Reconhece face

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,52 @@
 const cam = document.getElementById('cam')
 const startVideo = () => {
     navigator.mediaDevices.enumerateDevices()
-    .then(devices => {
-        if (Array.isArray(devices)) {
-            devices.forEach(device => {
-                if (device.kind === 'videoinput') {
-                    if (device.label.includes('')) {
-                        navigator.getUserMedia(
-                            { video: {
-                                deviceId: device.deviceId
-                            }},
-                            stream => cam.srcObject = stream,
-                            error => console.error(error)
-                        )
+        .then(devices => {
+            if (Array.isArray(devices)) {
+                devices.forEach(device => {
+                    if (device.kind === 'videoinput') {
+                        if (device.label.includes('')) {
+                            navigator.getUserMedia({
+                                    video: {
+                                        deviceId: device.deviceId
+                                    }
+                                },
+                                stream => cam.srcObject = stream,
+                                error => console.error(error)
+                            )
+                        }
                     }
-                }
-            })
+                })
+            }
+        })
+}
+
+const loadLabels = () => {
+    const labels = ['Douglas Braga']
+    return Promise.all(labels.map(async label => {
+        const descriptions = []
+        for (let i = 1; i <= 5; i++) {
+            const img = await faceapi.fetchImage(`/assets/lib/face-api/labels/${i}.jpg`)
+            const detections = await faceapi
+                .detectSingleFace(img)
+                .withFaceLandmarks()
+                .withFaceDescriptor()
+            descriptions.push(detections.descriptor)
         }
-    })
+        return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    }))
 }
 
 //api de redes neurais
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/assets/lib/face-api/models'),//square
-    faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/face-api/models'),//traços
-    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/face-api/models'),//reconhecimento
-    faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/face-api/models'),//expressoes faciais
-    faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/face-api/models'),//idade
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/face-api/models'),//usada internamente pra desenhar
+    faceapi.nets.tinyFaceDetector.loadFromUri('/assets/lib/face-api/models'), //square
+    faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/face-api/models'), //traços
+    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/face-api/models'), //reconhecimento
+    faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/face-api/models'), //expressoes faciais
+    faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/face-api/models'), //idade
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/face-api/models'), //usada internamente pra desenhar
 ]).then(startVideo)
+
 
 cam.addEventListener('play', async () => {
     const canvas = faceapi.createCanvasFromMedia(cam)
@@ -36,6 +54,8 @@ cam.addEventListener('play', async () => {
         width: cam.width,
         height: cam.height
     }
+    const labels = await loadLabels()
+
     faceapi.matchDimensions(canvas, canvasSize)
     document.body.appendChild(canvas)
     setInterval(async () => {
@@ -45,9 +65,35 @@ cam.addEventListener('play', async () => {
                 new faceapi.TinyFaceDetectorOptions()
             )
             .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender()
             .withFaceDescriptors()
+
         const resizedDetections = faceapi.resizeResults(detections, canvasSize)
+        const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+
+        const results = resizedDetections.map(d =>
+            faceMatcher.findBestMatch(d.descriptor)
+        )
+
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        resizedDetections.forEach(detection => {
+            const { age, gender, genderProbability } = detection
+            new faceapi.draw.DrawTextField([
+                `${parseInt(age, 10)} years`,
+                `${gender} (${parseInt(genderProbability * 100, 10)})`
+            ], detection.detection.box.topRight).draw(canvas)
+        })
+
+        results.forEach((result, index) => {
+            const box = resizedDetections[index].detection.box
+            const { label, distance } = result
+            new faceapi.draw.DrawTextField([
+                `${label} (${parseInt(distance * 100, 10)})`
+            ], box.bottomRight).draw(canvas)
+        })
     }, 100)
 })
